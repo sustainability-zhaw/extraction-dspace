35eadf594109e13ae6500ddd4d6e5c322ab1a206
Stefan Glüge <glue@zhaw.ch>
2023-01-16 15:22:29 +0100

    changed updateDate to last date found in dgraph DB

--- src/local_dev/get_data_from_digcol_add_to_graphdb.py
+++ src/local_dev/get_data_from_digcol_add_to_graphdb.py
@@ -1,10 +1,36 @@
 import requests
-import json
+# import json
 import re
 from bs4 import BeautifulSoup
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 from datetime import datetime
+
+
+async def get_last_dgraph_update_timestamp(client):
+    """
+    The get_last_dgraph_update_timestamp function returns the last time that Dgraph was updated.
+    It does this by querying the dgraph database for a queryInfoObject with a dateUpdate field, and then returning 
+    the value of that field.
+    
+    :param client: Access the dgraph api
+    :return: The date of the last update to the dgraph database / else None
+    """
+    query = gql(
+        """
+        query {
+            queryInfoObject(order: {desc: dateUpdate}, first: 1) {
+                dateUpdate     
+            }
+        }
+        """
+    )
+    result = await client.execute_async(query)
+    # print(result)
+    if len(result['queryInfoObject']) > 0:
+        return result['queryInfoObject'][0]['dateUpdate']
+    else:
+        return None
 
 
 def get_current_utc_datetime():
@@ -55,7 +81,7 @@ def get_single_chunk_oai_records_by_date(oai_url, datestamp=None, resumption_tok
     """
 
     if not datestamp: # set some default datestamp
-        datestamp = '1900-01-01'
+        datestamp = '1900-01-01T00:00:00Z'
 
     verb = 'ListRecords'
     if resumption_token is None: # no resumtion token, so get first chunk
@@ -142,13 +168,13 @@ def gen_record_dict(record):
     record_dc_description_list = get_entity_from_xml_record_entity(record, 'dc:description')
     record_dc_date_list = get_entity_from_xml_record_entity(record, 'dc:date')
     record_dc_type_list = get_entity_from_xml_record_entity(record, 'dc:type')
-    # record_dc_identifier_list = get_entity_from_xml_record_entity(record, 'dc:identifier')
+    #record_dc_identifier_list = get_entity_from_xml_record_entity(record, 'dc:identifier')
     record_dc_language_list = get_entity_from_xml_record_entity(record, 'dc:language')
     # record_dc_rights_list = get_entity_from_xml_record_entity(record, 'dc:rights')
     # record_dc_publisher_list = get_entity_from_xml_record_entity(record, 'dc:publisher')
     # record_dc_relation_list = get_entity_from_xml_record_entity(record, 'dc:relation')
     # record_dc_publisher_list = get_entity_from_xml_record_entity(record, 'dc:publisher')
-
+    record_datestamp = record.datestamp.contents[0]
     # get title of the record
     if len(record_titel_list) > 0:
         record_title = record_titel_list[0]
@@ -186,6 +212,7 @@ def gen_record_dict(record):
 
     record_dict = {
         'title': record_title,
+        'datestamp': record_datestamp,
         'authors': [{'fullname': record_dc_creator_list[i]} for i in range(len(record_dc_creator_list))], # [{ fullname: "Föhn, Martina" }]
         'persons': [], #[{ LDAPDN: "cn=fhia" }]
         'abstract': record_abstract,
@@ -193,7 +220,6 @@ def gen_record_dict(record):
         'keywords':  [{'name': record_keyword_list[i]} for i in range(len(record_keyword_list))], # [{ name: "Forest therapy" }, { name: "Health" }, { name: "Mindfulness" }, { name: "Distress" }, { name: "Forest medicine" }, { name: "Shinrin yoku" }, { name: "Cortisol" }, { name: "Forest bathing" }]
         'class': [{'id': record_class_list[i].split(':')[0], 'name': record_class_list[i].split(':')[1]} for i in range(len(record_class_list))], # [{ id: "615" name: "Pharmakologie und Therapeutik" }]
         'link': record_url,# "https://digitalcollection.zhaw.ch/handle/11475/23944",
-        'sdgs': [],
         'language': record_language, #"de",
         'category': {'name': 'publications'},
         'subtype':  {'name': record_subtype},
@@ -239,71 +265,6 @@ def gen_mutation_string_for_classes(classes):
     """
     classes_string = '[' + ', '.join(['{id: "' + class_['id'] + '", name: "' + class_['name'] + '"}' for class_ in classes]) + ']'
     return classes_string
-
-
-def gen_pub_mutation_string(title, authors, abstract,link, year, language, keywords, classes, subtype):
-    """
-    The gen_pub_mutation_string function takes in a dictionary of information about a publication and returns
-    a string that can be used to add the publication to the database. The string is formatted in the following way:
-    mutation {
-        addInfoObject(input: [{
-            title: "Forest bathing and its effects on stress and health: A systematic review",
-            authors: [{fullname: "Cantoni, Marco"}, {fullname: "Holzer, Lorenz"}],
-            link: "https://digitalcollection.zhaw.ch/handle/11475/23944",
-            year: 2022,
-            keywords: [{name: "Forest therapy"}, {name: "Health"}],
-            class: [{id: "615", name: "Pharmakologie und Therapeutik"}],
-            language: "de",
-            category: {name: "publications"},
-            subtype: {name: "Journal Article"}
-        }],
-            upsert: true) {
-                infoObject {
-                    title
-                    link
-                    year
-                }
-            }    
-        }       
-    
-    :param title: Set the title of the publication
-    :param authors: Generate the mutation string for authors
-    :param abstract: Generate the abstract field of the publication
-    :param link: Link the publication to a specific page on the website
-    :param year: Filter the publications
-    :param language: Determine the language of the publication
-    :param keywords: Generate a string that can be used in the mutation query
-    :param classes: Specify the class of the publication
-    :param subtype: Specify the type of publication
-    :return: The string for the mutation that will add a publication to the database
-    """
-    
-    query_string = """
-        mutation {
-            addInfoObject(input: [{
-        """ + \
-        'title: "' + title + '",\n' + \
-        'authors: ' + gen_mutation_string_for_authors(authors) + ',\n' + \
-        'link: "' + link + '",\n' + \
-        'year: ' + str(year) + ',\n' + \
-        'abstract: "' + abstract + '",\n' + \
-        'language: "' + language + '",\n' + \
-        'keywords: ' + gen_mutation_string_for_keywords(keywords) + ',\n' + \
-        'class: ' + gen_mutation_string_for_classes(classes) + ',\n' + \
-        'category: { name: "publications" },\n' + \
-        'subtype: { name: "' + subtype + '" }' + \
-        """
-           }],
-            upsert: true) {
-                infoObject {
-                    title
-                    link
-                    year
-                }
-            }    
-        }   
-        """
-    return query_string
 
 
 def gen_pub_mutation_string_update(title, authors, abstract,link, year, language, keywords, classes, subtype, update_datetime):
@@ -373,55 +334,7 @@ def gen_pub_mutation_string_update(title, authors, abstract,link, year, language
     return query_string
 
 
-async def add_records_to_graphdb(oaixml, client):
-    """
-    The add_records_to_graphdb function takes in a chunk of records and adds them to the graphdb database.
-    :param oaixml: A chunk of records
-    :return: (# of inserted records, # of deleted records)
-    """
-    
-    inserted_records = 0
-    deleted_records = 0
-
-    # add chunk of records to the database
-    for record in oaixml.find_all('record'):
-        print(record)
-        
-        # check header if record is deleted ... indicated by tag: status = deleted
-        record_deleted = False
-        if len(record.header.attrs) > 0:
-            if record.header['status'] == 'deleted':
-                print('Record is deleted')
-                record_deleted = True
-                deleted_records += 1
-
-        if not record_deleted:  
-            record_dict = gen_record_dict(record)  # extract information for current record
-
-            my_query = gen_pub_mutation_string(
-                title = record_dict['title'].encode('utf-8').decode('utf-8'),
-                authors = record_dict['authors'],
-                abstract = record_dict['abstract'].encode('utf-8').decode('utf-8'),
-                link = record_dict['link'],
-                year = record_dict['year'],
-                language = record_dict['language'],
-                keywords = record_dict['keywords'],
-                classes = record_dict['class'],
-                subtype = record_dict['subtype']['name']
-                )
-            
-            # import io
-            # with io.open("my_query.gql",'w', encoding='utf-8') as f:
-                # f.write(my_query)
-        
-            query = gql(my_query)
-            result = await client.execute_async(query)
-            # print(result)
-            inserted_records += 1
-    return inserted_records, deleted_records
-
-
-async def add_records_to_graphdb_with_updateDate(oaixml, update_datetime, client):
+async def add_records_to_graphdb_with_updateDate(oaixml, client):
     """
     The add_records_to_graphdb function takes in a chunk of records and adds them to the graphdb database.
     :param oaixml: A chunk of records
@@ -456,7 +369,7 @@ async def add_records_to_graphdb_with_updateDate(oaixml, update_datetime, client
                 keywords = record_dict['keywords'],
                 classes = record_dict['class'],
                 subtype = record_dict['subtype']['name'],
-                update_datetime = update_datetime  # Note that the time must be in the format of "YYYY-MM-DDTHH:MM:SSZ" and must include the "Z" at the end to indicate that it is in UTC time
+                update_datetime = record_dict['datestamp']  # Note that the time must be in the format of "YYYY-MM-DDTHH:MM:SSZ" and must include the "Z" at the end to indicate that it is in UTC time
                 )
             
             # import io
@@ -481,15 +394,17 @@ client = Client(transport=transport, fetch_schema_from_transport=True)
 total_inserted_records = 0
 total_deleted_records = 0
 
-# get the first chunk of records
-dgraph_update_datetime = get_current_utc_datetime()
-publication_datestamp = '2023-01-13' 
+# get_last_dgraph_update_timestamp
+last_update_timestamp = await get_last_dgraph_update_timestamp(client)
+last_update_timestamp == None
 
-oaixml = get_single_chunk_oai_records_by_date(oai_url, datestamp=publication_datestamp)
-len(oaixml.find_all('record'))
+oaixml = get_single_chunk_oai_records_by_date(oai_url, datestamp=last_update_timestamp)
+token = oaixml.resumptionToken
+
+# len(oaixml.find_all('record'))
 
 # add the first chunk of records to the database
-inserted_records, deleted_records = await add_records_to_graphdb_with_updateDate(oaixml, update_datetime=dgraph_update_datetime, client=client)
+inserted_records, deleted_records = await add_records_to_graphdb_with_updateDate(oaixml, client=client)
 total_inserted_records += inserted_records
 total_deleted_records += deleted_records
 
@@ -498,7 +413,7 @@ while token is not None:
     oaixml = get_single_chunk_oai_records_by_date(oai_url, resumption_token=token)
     token = oaixml.resumptionToken
     # add the next chunk of records to the database
-    inserted_records, deleted_records = await add_records_to_graphdb_with_updateDate(oaixml, update_datetime=dgraph_update_datetime, client=client)
+    inserted_records, deleted_records = await add_records_to_graphdb_with_updateDate(oaixml, client=client)
     total_inserted_records += inserted_records
     total_deleted_records += deleted_records
 
@@ -578,6 +493,25 @@ print('Total number of deleted records: ', total_deleted_records)
 #     )
 #     result = await session.execute(query, variable_values={'pubyear': 2019})
 #     print(result)
+
+# get last update timestamp from the graph db
+# async with Client(
+#         transport=transport,
+#         fetch_schema_from_transport=True,
+#     ) as session:
+#     query = gql(
+#         """
+#         query {
+#             queryInfoObject(order: {desc: dateUpdate}, first: 1) {
+#                 dateUpdate     
+#             }
+#         }
+#         """
+#     )
+#     result = await session.execute(query)
+    
+#     print(result['queryInfoObject'][0]['dateUpdate'])
+
 
 
 # result['queryInfoObject'][0]
