e73d77f6e16951365640854a655793ec188bd9e8
Christian Glahn <cpglahn@gmail.com>
2023-01-25 07:21:48 +0100

    run profiling logs

--- src/main.py
+++ src/main.py
@@ -7,14 +7,17 @@ logging.basicConfig(format="%(levelname)s: %(name)s: %(asctime)s: %(message)s", 
 
 logger = logging.getLogger('extract-dspace-loop')
 
-logger.info('start iteration')
 limit_batch = settings.LIMIT_BATCH  # -1, no limit ... process all batches
 batch_count = 0
 resumption_token = None
 
 while (limit_batch == -1) or (limit_batch > 0 and batch_count < limit_batch): # limit number of batches to be processed:
+    logger.info('start iteration') # for server logs and profiling, need to run right before the hookup.run().
     resumption_token = await hookup.run(resumption_token) # ask for a batch of records and add them to the graph database
     # resumption_token = None if there is only a single batch to be processed
+    logger.info('complete iteration') # for server logs and profiling, need to run right after the hookup.run().
+    
+    # house keeping
     batch_count += 1 # increment batch counter
     time.sleep(settings.OAI_REQUEST_INTERVAL) # wait before asking for the next batches
     if resumption_token is None:
