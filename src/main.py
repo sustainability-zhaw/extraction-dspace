08871be1b86fde38cbf540907e520efd35ca1b5e
Christian Glahn <cpglahn@gmail.com>
2023-01-25 07:28:05 +0100

    only sleep once per loop

--- src/main.py
+++ src/main.py
@@ -14,13 +14,18 @@ resumption_token = None
 while (limit_batch == -1) or (limit_batch > 0 and batch_count < limit_batch): # limit number of batches to be processed:
     logger.info('start iteration') # for server logs and profiling, need to run right before the hookup.run().
     resumption_token = await hookup.run(resumption_token) # ask for a batch of records and add them to the graph database
-    # resumption_token = None if there is only a single batch to be processed
     logger.info('complete iteration') # for server logs and profiling, need to run right after the hookup.run().
-    
+
     # house keeping
     batch_count += 1 # increment batch counter
-    time.sleep(settings.OAI_REQUEST_INTERVAL) # wait before asking for the next batches
+    
+    # resumption_token = None if there are no more batches to be processed
     if resumption_token is None:
         logger.info('finish iteration after ' + str(batch_count) + ' batches') 
-        time.sleep(settings.PUBDB_UPDATE_INTERVAL)
+        sleepTimeout = settings.PUBDB_UPDATE_INTERVAL
         batch_count = 0 # reset batch counter for
+    else:
+        logger.info('prepare for another batch') 
+        sleepTimeout = settings.OAI_REQUEST_INTERVAL # wait before asking for the next batches
+    
+    time.sleep(sleepTimeout) # wait before asking for the next batches
