f5ec55dacff0e7926628b5a82d29a95ec1ec0934
Christian Glahn <cpglahn@gmail.com>
2023-01-25 13:16:05 +0100

    use the correct ENV names

--- src/settings.py
+++ src/settings.py
@@ -8,7 +8,7 @@ import os
 TARGET_HOST = os.getenv('TARGET_HOST', '') # url to the oai-pmh api'
 TARGET_PATH = os.getenv('TARGET_PATH', '/oai/request/')
 DB_HOST = os.getenv('DB_HOST', 'http://localhost:8080') # url to the grapg database
-DB_PATH = os.getenv('DB_HOST', '/graphql') # url to the grapg database
+DB_PATH = os.getenv('DB_PATH', '/graphql') # url to the grapg database
 PUBDB_UPDATE_INTERVAL = int(os.getenv('PUBDB_UPDATE_INTERVAL', 2592000)) # time to wait between updates of the publication database
 OAI_REQUEST_INTERVAL = int(os.getenv('OAI_REQUEST_INTERVAL', 0)) # time to wait between requests to the oai-pmh api
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
